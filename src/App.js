5e6dfb4b54e219a0a62e3885c038d0b516ff0e18
smirnov_sv <smistav@ya.ru>
2022-01-24 18:41:24 +0300

    flex grow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { useState } from 'react'
 import s from './app.module.scss'
-import Modal from './components/modal/modal'
 
 function App() {
-  const [open, setOpen] = useState(false)
   return (
     <div className={s.component}>
-      <button type='button' onClick={() => setOpen(!open)}>
-        Open window
-      </button>
-      <Modal open={open} onClose={() => setOpen(false)} />
+      <ul className={s.itemHeader}>
+        <li className={s.item}>kjkjhkj</li>
+        <li className={s.item}>fsffwfwe</li>
+        <li className={s.item}>wefwe</li>
+        <li className={s.item}>wefwef</li>
+        <li className={s.item}>wefwefwef</li>
+        <li className={s.item}>wefwefwef</li>
+      </ul>
     </div>
   )
 }

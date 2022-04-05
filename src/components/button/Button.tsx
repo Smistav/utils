e725388f68fb3051dc535c0795d9a223d1d69cf6
Smirnov Stanislav <ssmirnov2@sitronics.com>
2022-04-05 15:13:37 +0300

    button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,12 +2,14 @@ import style from './Button.module.scss'
 import cn from 'classnames'
 import React from 'react'
 
+type ButtonType = 'button' | 'submit' | 'reset' | undefined
+
 type ButtonProps = {
   children: React.ReactNode
   classes?: string[]
   onClick?: (evt: React.MouseEvent<HTMLElement>) => void
   ref?: React.RefObject<HTMLButtonElement>
-  type?: any
+  type: ButtonType
 }
 export const Button = React.memo(({ children, classes, onClick, type, ref }: ButtonProps) => {
   return (
